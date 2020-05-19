56023e5ca1beb92ce9f0cc4839604788b8e0d1a4
Aled Jackson <aledjackson24@gmail.com>
2020-05-19 21:23:27 +0100

    created basic ui for title page and languages / tools page

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+def intro():
+    i = """Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis """
+
+    return i
--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import intro
 
 # Create your views here.
 def index(request) -> HttpResponse:
-    return render(request, 'home/index.html',{})
+    return render(request, 'home/index.html',{"intro" : intro()})
